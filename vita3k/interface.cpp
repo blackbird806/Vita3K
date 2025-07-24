79c8b8efbd270f9d52f548cde43e6e9d6971433f
blackbird806 <blackbirdcry806@gmail.com>
2025-07-24 18:27:16 +0200

    oops forgot that file

--- vita3k/interface.cpp
+++ vita3k/interface.cpp
@@ -719,6 +719,8 @@ bool handle_events(EmuEnvState &emuenv, GuiState &gui) {
                 toggle_texture_replacement(emuenv);
             if (event.key.keysym.scancode == emuenv.cfg.keyboard_take_screenshot && !gui.is_key_capture_dropped)
                 take_screenshot(emuenv);
+            if (event.key.keysym.scancode == emuenv.cfg.pinch_modifier && !gui.is_key_capture_dropped)
+                pinch_modifier(true);
 
             if (sce_ctrl_btn != 0) {
                 if (last_buttons.contains(sce_ctrl_btn)) {
@@ -732,6 +734,8 @@ bool handle_events(EmuEnvState &emuenv, GuiState &gui) {
         }
         case SDL_KEYUP:
             gui.is_key_locked = false;
+            if (event.key.keysym.scancode == emuenv.cfg.pinch_modifier)
+                pinch_modifier(false);
             break;
 
         case SDL_MOUSEMOTION:
